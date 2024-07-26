d970dd39e2562385445acc3a3c810d26eb8d854b
mnothman <mohammadothman113@gmail.com>
2024-07-26 12:05:26 -0700

    shuffling images

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ try:
     dataset = foz.load_zoo_dataset(
         "open-images-v7",
         split="validation",
-        # dataset_dir=IMAGE_DIR,
-        # dataset_dir="/data/open-images-v7",
         dataset_dir="data",
-        max_samples=20,  # limit 20 imgs for testing
+        max_samples=100,  # limit for testing
     )
 except KeyError as e:
     print(f"Error downloading dataset: {e}")
@@ -24,23 +22,28 @@ except KeyError as e:
 
 images = []
 
+if dataset: 
+    dataset = dataset.shuffle()
 
-# prepare images for rendering
-for sample in dataset:
-    image_path = sample.filepath
-    image_filename = os.path.basename(image_path)
+    selected_samples = dataset.take(20)
 
-    if hasattr(sample, 'ground_truth') and sample.ground_truth.detections:
-        label = sample.ground_truth.detections[0].label
-    else:
-        label = "No Label"
-    images.append({
-        "id": sample.id,
-        "url": f"/images/{image_filename}",
-        "label": label
-    })
+    # prepare images for rendering
+    for sample in dataset:
+        image_path = sample.filepath
+        image_filename = os.path.basename(image_path)
 
-    print(f"Sample ID: {sample.id}, Label: {label}, Image Path: {image_path}")
+        if hasattr(sample, 'ground_truth') and sample.ground_truth.detections:
+            label = sample.ground_truth.detections[0].label
+        else:
+            label = "No Label"
+            
+        images.append({
+            "id": sample.id,
+            "url": f"/images/{image_filename}",
+            "label": label
+        })
+
+        print(f"Sample ID: {sample.id}, Label: {label}, Image Path: {image_path}")
 
 user_interactions = []
 
